88f45e6d633e9c1ac894eea841164571050d9939
Jaideep Sundaram <jai.python3@gmail.com>
2022-03-26 15:02:08 -0400

    ADDS use of reqparse

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -18,6 +18,14 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+    
     @jwt_required()
     def get(self, name):
         # for item in items:
@@ -31,7 +39,8 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': "An item with name '{name}' already exists"}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -43,6 +52,8 @@ class Item(Resource):
 
 
     def put(self, name):
+        data = Item.parser.parse_args()
+
         global items
         data = request.get_json()
         item = next(filter(lambda x: x['name'] == name, items), None)
