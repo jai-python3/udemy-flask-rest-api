efb2577a17238743a3c24f859f070e0e7a58bd4e
Jaideep Sundaram <jai.python3@gmail.com>
2022-03-26 21:51:57 -0400

    ADD support to retrieve item from items table ADD support to insert item into items table ADD support to delete item from items table

--- item.py
+++ item.py
@@ -16,6 +16,14 @@ class Item(Resource):
     
     @jwt_required()
     def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+
+        
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -23,26 +31,40 @@ class Item(Resource):
         result = cursor.execute(query, (name,))
         row = result.fetchone()
         connection.close()
-        
+
         if row:
             return {'item': {'name': row[0], 'price': row[1]}}
-        return {'message': 'Item not found'}, 404
-
-        
 
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
+        if self.find_by_name(name):
             return {'message': "An item with name '{name}' already exists"}, 400
 
         data = Item.parser.parse_args()
 
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+        
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items values (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+        
+        connection.commit()
+        connection.close()
+
+
         return item, 201
 
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name = ?"
+        cursor.execute(query, (name,))
+        
+        connection.commit()
+        connection.close()
+
         return {'message': 'Item deleted'}
 
 
