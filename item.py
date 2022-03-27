9db9c91b2464d69dfd292691a33b697e369fd42a
Jaideep Sundaram <jai.python3@gmail.com>
2022-03-26 22:09:35 -0400

    ADD support to update item

--- item.py
+++ item.py
@@ -43,6 +43,15 @@ class Item(Resource):
 
         item = {'name': name, 'price': data['price']}
         
+        try:
+            self.insert(item)
+        except:
+            return {'message': 'An error occurred inserting the item.'}, 500  # Internal server error
+
+        return item, 201
+
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -52,8 +61,6 @@ class Item(Resource):
         connection.commit()
         connection.close()
 
-
-        return item, 201
 
     def delete(self, name):
         connection = sqlite3.connect('data.db')
@@ -71,15 +78,35 @@ class Item(Resource):
     def put(self, name):
         data = Item.parser.parse_args()
 
-        global items
-        data = request.get_json()
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
+
         if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
+            try:
+                self.insert(updated_item)
+            except:
+                return {'message': 'An error occurred inserting the item.'}, 500  # Internal server error
+
         else:
-            item.update(data)
-        return item
+            try:
+                self.update(updated_item)
+            except:
+                return {'message': 'An error occurred updating the item.'}, 500  # Internal server error
+
+        return updated_item
+
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price = ? WHERE name = ?"
+        cursor.execute(query, (item['price'], item['name']))
+        
+        connection.commit()
+        connection.close()
+
 
 
 class ItemList(Resource):
