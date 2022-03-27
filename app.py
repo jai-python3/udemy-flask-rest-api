0f25986cd91c66440f5d88a12b1647d58e772ddc
Jaideep Sundaram <jai.python3@gmail.com>
2022-03-26 20:57:20 -0400

    ADD support for creating SQLite table users ADD support for new user registration

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+from user import UserRegister
+
 
 PORT = 5000
 
@@ -71,6 +73,6 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-
+api.add_resource(UserRegister, '/register')
 
 app.run(port=PORT, debug=True)
